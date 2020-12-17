a28124f1d8c0e5d1cc83fa1ff4edd7c276c30f54
swilcox21 <swilcox21@gmail.com>
2020-12-17 22:42:06 +0000

    pushing my backend first push

--- src/main.py
+++ src/main.py
@@ -30,14 +30,96 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
+    users = User.query.all()
+    users = list(map(lambda x: x.serialize(), users))
+    return jsonify(users), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user', methods=['POST'])
+def post_user():
+    body = request.get_json()
+    new_user = User.query.filter_by(email=body['email']).first()
+    if new_user is not None:
+        raise APIException('this user is already registered', status_code = 404)
+    new_user = User(first_name = body['firstName'],
+                    last_name = body['lastName'],
+                    email = body['email'],
+                    password = body['password'],
+                    street_address = body['streetAddress'],
+                    city = body['city'],
+                    the_state = body['theState'],
+                    zip_code = body['zipCode'],
+                    is_active = body['is_active'])
+    db.session.add(new_user)
+    db.session.commit()
+    user = User.query.filter_by(email=body['email']).first()
+    print('UNSERIALIZED', user)
+    # user = list(map(lambda x: x.serialize(), user))
+    print('SERIALIZED', user)
+    return jsonify(user.serialize()), 201
 
-    return jsonify(response_body), 200
+@app.route('/todo/<username>/<int:id>', methods=['PUT'])
+def put_todo(username, id):
+    body = request.get_json()
+    todo_item = Todo.query.get(id)
+    print("MYTODOITEM", todo_item)
+    todo_item.done = body['done']
+    todo_item.label = body['label']
+    db.session.commit()
+    updated_item = Todo.query.get(id)
+    updated_item = updated_item.serialize()
+    return jsonify(updated_item), 200
+
+@app.route('/user/<email>/<int:id>', methods=['DELETE'])
+def delete_user(email, id):
+    user = User.query.get(id)
+    if user is None:
+        raise APIException('user does not exist', status_code = 400)
+    db.session.delete(user)
+    db.session.commit()
+    user = User.query.filter_by(email = email)
+    # user = list(map(lambda x: x.serialize(), User))
+    return jsonify(user), 202
+
+# @app.route('/cart/<email>', methods=['GET'])
+# def get_cart():
+#     cart = Cart.query.all()
+#     cart = list(map(lambda x: x.serialize(), cart))
+#     return jsonify(cart), 200
+
+# @app.route('/cart/<email>', methods=['POST'])
+# def post_cart_item(email,id):
+#     body = request.get_json()
+#     new_cart = Cart.query.filter_by(email=body['email']).first()
+#     if new_cart is not None:
+#         raise APIException('this item is already in your cart', status_code = 404)
+#     new_cart = Cart(email = body['email'],
+#                     name = body['name'],
+#                     unit_price = body['unitPrice'],
+#                     sub_total = body['subTotal'],
+#                     color = body['color'],
+#                     size = body['size'],
+#                     units = body['units'],
+#                     image = body['image'])
+#     db.session.add(new_cart)
+#     db.session.commit()
+#     cart = Cart.query.filter_by(email=body['email']).first()
+#     print('UNSERIALIZED', cart)
+#     print('SERIALIZED', cart)
+#     return jsonify(cart.serialize()), 201
+
+# @app.route('/cart/<email>/<int:id>', methods=['DELETE'])
+# def delete_cart_item(email, id):
+#     cart_item = Cart.query.get(id)
+#     if cart_item is None:
+#         raise APIException('item does not exist', status_code = 400)
+#     db.session.delete(cart_item)
+#     db.session.commit()
+#     cart = Cart.query.filter_by(email = email)
+#     cart = list(map(lambda x: x.serialize(), Cart))
+#     return jsonify(Cart), 202
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,105 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), unique=False, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    street_address = db.Column(db.String(120), unique=False, nullable=True)
+    city = db.Column(db.String(120), unique=False, nullable=True)
+    the_state = db.Column(db.String(120), unique=False, nullable=True)
+    zip_code = db.Column(db.String(120), unique=False, nullable=True)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            "email": self.email,
+            "streetAddress": self.street_address,
+            "city": self.city,
+            "theState": self.the_state,
+            "zipCode": self.zip_code,
+            "isActive": self.is_active
+            # do not serialize the password, its a security breach
+        }
+
+class History(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    unit_price = db.Column(db.Integer, unique=False, nullable=False)
+    sub_total = db.Column(db.Integer, unique=False, nullable=False)
+    color = db.Column(db.String(120), unique=False, nullable=True)
+    size = db.Column(db.String(120), unique=False, nullable=True)
+    units = db.Column(db.Integer, unique=False, nullable=False)
+    image = db.Column(db.String(5000), unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<History %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "unitPrice": self.unit_price,
+            "subTotal": self.sub_total,
+            "color": self.color,
+            "size": self.size,
+            "units": self.units,
+            "image": self.image,
+            # do not serialize the password, its a security breach
+        }
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    unit_price = db.Column(db.Integer, unique=False, nullable=False)
+    sub_total = db.Column(db.Integer, unique=False, nullable=False)
+    color = db.Column(db.String(120), unique=False, nullable=True)
+    size = db.Column(db.String(120), unique=False, nullable=True)
+    units = db.Column(db.Integer, unique=False, nullable=False)
+    image = db.Column(db.String(5000), unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<Cart %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            "unitPrice": self.unit_price,
+            "subTotal": self.sub_total,
+            "color": self.color,
+            "size": self.size,
+            "units": self.units,
+            "image": self.image,
+            # do not serialize the password, its a security breach
+        }
+
+class Transactions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(120), unique=False, nullable=False)
+    card = db.Column(db.Integer, unique=False, nullable=False)
+    amount = db.Column(db.Integer, unique=False, nullable=False)
+    status = db.Column(db.String(30), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Transactions %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "date": self.date,
+            "card": self.card,
+            "amount": self.amount,
+            "status": self.status,
             # do not serialize the password, its a security breach
         }
